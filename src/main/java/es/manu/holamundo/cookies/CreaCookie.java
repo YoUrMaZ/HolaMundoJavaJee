3d372a156ca625ff5fe789143d8fb82a7e1d92ca
Manuel Ruz <manuel22ene@gmail.com>
2021-11-04 15:01:00 +0100

    04-11-2021 (mañana)

--- src/main/java/es/manu/holamundo/cookies/CreaCookie.java
+++ src/main/java/es/manu/holamundo/cookies/CreaCookie.java
@@ -27,7 +27,7 @@ public class CreaCookie extends HttpServlet {
   protected void procesaSolicitud(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html;chagrset=UTF-8");
         PrintWriter out = response.getWriter();
         // se inicializa un objeto Cookie
         Cookie unaCookie;
